69f2e95c5214a7d39995512754a4ec00d4fc07ad
Thura Freelance <mozet89@gmail.com>
2024-01-12 17:23:04 +0630

    converting modal to compound component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -4,19 +4,39 @@ import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
 
 const AddCabin = () => {
-  const [isOpenModel, setIsOpenModel] = useState(false);
   return (
-    <div>
-      <Button onClick={() => setIsOpenModel((show) => !show)}>
-        Add New Cabin
-      </Button>
-      {isOpenModel && (
-        <Modal onClose={() => setIsOpenModel(false)}>
-          <CreateCabinForm onCloseModel={() => setIsOpenModel(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add New Cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      {/* <Modal.open>
+        <Button>Show Table</Button>
+      </Modal.open>
+      <Modal.window>
+        <CreateCabinForm />
+      </Modal.window> */}
+    </Modal>
   );
 };
+
+// const AddCabin = () => {
+//   const [isOpenModel, setIsOpenModel] = useState(false);
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModel((show) => !show)}>
+//         Add New Cabin
+//       </Button>
+//       {isOpenModel && (
+//         <Modal onClose={() => setIsOpenModel(false)}>
+//           <CreateCabinForm onCloseModel={() => setIsOpenModel(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// };
 
 export default AddCabin;
--- src/ui/Form.jsx
+++ src/ui/Form.jsx
@@ -4,14 +4,12 @@ const Form = styled.form`
   ${(props) =>
     props.type === "regular" &&
     css`
-      background-color: #ddd;
       padding: 2.4rem 4rem;
 
       /* Box */
       background-color: var(--color-grey-0);
       border: 1px solid var(--color-grey-100);
       border-radius: var(--border-radius-md);
-      border: 1px solid red;
     `}
 
   ${(props) =>
